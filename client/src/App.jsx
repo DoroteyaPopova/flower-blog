483ae5142d4399f1ab55ad8a86b2f484de815edd
SnowShine7 <snowsocial7@gmail.com>
2025-04-03 22:18:16 +0300

    imported home and login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 
 import "./App.css";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.withCredentials = true;
