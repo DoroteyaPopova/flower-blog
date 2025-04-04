ec0ba90e9923ec2f61b8c0da2d43a9832f4e677e
SnowShine7 <snowsocial7@gmail.com>
2025-04-04 19:08:41 +0300

    updated routes of App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 
 import "./App.css";
+
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Nav from "./components/Navigation/Nav";
 
 axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.withCredentials = true;
 
 function App() {
    return (
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/login" element={<Login />} />
-      </Routes>
+      <>
+         <Nav />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+         </Routes>
+      </>
    );
 }
 
