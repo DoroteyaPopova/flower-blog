442f2f4049294b4a1e206c1db2cc5340fc21a5fb
SnowShine7 <snowsocial7@gmail.com>
2025-04-05 20:53:28 +0300

    added user context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import { UserContextProvider } from "./context/userContext";
 
 import "./App.css";
 
@@ -7,20 +9,32 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Nav from "./components/Navigation/Nav";
+import Logout from "./components/Logout/Logout";
+import Catalog from "./components/Catalog/Catalog";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.withCredentials = true;
 
 function App() {
    return (
-      <>
+      <UserContextProvider>
          <Nav />
          <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/catalog" element={<Catalog />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage />} />
+
+            <Route element={<ProtectedRoute />}>
+               <Route path="/logout" element={<Logout />} />
+            </Route>
          </Routes>
-      </>
+         <Toaster position="bottom-right" toastOptions={{ duration: 3000 }} />
+      </UserContextProvider>
    );
 }
 
