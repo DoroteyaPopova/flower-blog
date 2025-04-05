b727f0c7b8c6cedfd9405d8d81620ca408435c25
SnowShine7 <snowsocial7@gmail.com>
2025-04-05 20:53:01 +0300

    upgraded to dynamic navigation

--- client/src/components/Navigation/Nav.jsx
+++ client/src/components/Navigation/Nav.jsx
@@ -1,14 +1,27 @@
 import styles from "./Nav.module.css";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import { useContext } from "react";
 
 const Nav = () => {
+   const { user } = useContext(UserContext);
    return (
       <nav>
-         <div className={styles.centered}>
-            <Link to="/">Home</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
-         </div>
+         {user && (
+            <div className={styles.centered}>
+               <Link to="/">Home</Link>
+               <Link to="/catalog">Catalog</Link>
+               <Link to="/logout">Logout</Link>
+            </div>
+         )}
+         {!user && (
+            <div className={styles.centered}>
+               <Link to="/">Home</Link>
+               <Link to="/catalog">Catalog</Link>
+               <Link to="/register">Register</Link>
+               <Link to="/login">Login</Link>
+            </div>
+         )}
       </nav>
    );
 };
