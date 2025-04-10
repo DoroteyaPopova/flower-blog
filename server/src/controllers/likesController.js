f942798b4a448d036394d4460b43dcde1977bab7
SnowShine7 <snowsocial7@gmail.com>
2025-04-10 22:12:06 +0300

    add test for no flowers

--- server/src/controllers/likesController.js
+++ server/src/controllers/likesController.js
@@ -108,6 +108,8 @@ exports.getTopLikedFlowers = async (req, res) => {
             };
          })
       );
+
+      // const result = []; //No flowers test
       res.status(200).json(result);
    } catch (error) {
       res.status(500).json({ error: error.message });
